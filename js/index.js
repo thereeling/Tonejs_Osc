5e78c99075697e3c463af1693b13ce961cb85402
Nick Giuliani <nickg@pop-os.localdomain>
2022-04-28 22:44:38 -0400

    trying to get note to play

--- js/index.js
+++ js/index.js
@@ -1,18 +1,17 @@
-var ready = false;
-const osc = new Tone.Oscillator();
-osc.connect(Tone.Master);
 
-function draw() {
-    if (ready) {
+const osc = new Tone.Oscillator({
+    type: "square",
+    frequency: 440,
+    volume: -16
+}).toDestination();
+// ui({
+//     tone: osc,
+//     parent: document.querySelector("#content")
+// });
 
-    }
-    else {
+// bind the interface
 
-    }
-}
+document.getElementById("content").addEventListener("down", () => alert('yoo'));
 
-function mousePressed() {
-    if (!ready) {
-        ready = true;
-    }
-}
+// document.getElementById("content").addEventListener("down", () => osc.start());
+// document.getElementById("content").addEventListener("up", () => osc.stop());
